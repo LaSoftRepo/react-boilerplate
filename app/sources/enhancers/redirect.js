59f684ba344292c0cb3468b755719364cf871a3b
maxgraey <maxgraey@gmail.com>
2017-08-09 00:27:09 +0300

    add react-hoc

--- app/sources/enhancers/redirect.js
+++ app/sources/enhancers/redirect.js
@@ -9,7 +9,7 @@
  *   import { ifUserNotAuthorize } from './credentials';
  *
  *   @redirect(ifUserNotAuthorize, '/login')
- *   class SomeContainer extends Component {
+ *   class SomeContainer extends React.Component {
  *     constructor(props) {
  *       super(props);
  *     }
@@ -24,39 +24,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { replace, push } from 'react-router-redux';
+import reactHOC from 'react-hoc';
 import moize from 'moize';
 
-const redirect = (condition, toPath = '/login', replaceHistory = true) => WrappedComponent =>
-    @connect(({ router }) => ({ router }))
-    class extends Component {
-      componentWillMount() {
-        this.checkCondition(this.props);
-      }
+const redirect = (condition, toPath = '/login', replaceHistory = true) =>
+reactHOC(WrappedComponent =>
+  @connect(({ router }) => ({ router }))
+  class extends Component {
+    componentWillMount() {
+      this.checkCondition(this.props);
+    }
 
-      componentWillReceiveProps(nextProps) {
-        if (nextProps.router.location !== this.props.router.location) {
-          this.checkCondition(nextProps);
-        }
-      }
-
-      checkCondition(params) {
-        if (toPath && toPath.startsWith('/')) {
-          if (condition()) {
-            // TODO check if /login router exists
-            if (replaceHistory) {
-              params.dispatch(replace({ pathname: toPath }));
-            } else {
-              params.dispatch(push({ pathname: toPath }));
-            }
-          }
-        } else {
-          console.warn(`redirect: Invalid pathname "${ toPath }". Path should starts with slash "/"`);
-        }
-      }
-
-      render() {
-        return <WrappedComponent { ...this.props } />
+    componentWillReceiveProps(nextProps) {
+      if (nextProps.router.location !== this.props.router.location) {
+        this.checkCondition(nextProps);
       }
     }
+
+    checkCondition(params) {
+      if (toPath && toPath.startsWith('/')) {
+        if (condition()) {
+          // TODO check if /login router exists
+          if (replaceHistory) {
+            params.dispatch(replace({ pathname: toPath }));
+          } else {
+            params.dispatch(push({ pathname: toPath }));
+          }
+        }
+      } else {
+        console.warn(`redirect: Invalid pathname "${ toPath }". Path should starts with slash "/"`);
+      }
+    }
+
+    render() {
+      return <WrappedComponent { ...this.props } />
+    }
+  }
+)
 
 export default moize(redirect, { maxSize: 20 });
