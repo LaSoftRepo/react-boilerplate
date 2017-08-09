6549c4cb8b76710fa9bd43d335e07925da8623ea
maxgraey <maxgraey@gmail.com>
2017-08-09 16:51:23 +0300

    update Playground

--- app/sources/containers/Playground/index.js
+++ app/sources/containers/Playground/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { goBack } from 'react-router-redux';
 
-class Playground extends React.PureComponent {
+@connect()
+export default class Playground extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
@@ -13,22 +16,24 @@ class Playground extends React.PureComponent {
   }
 
   render() {
+    const { dispatch }    = this.props;
     const { selectValue } = this.state;
     return (
-      <div grid='rows' horizontally-aligned='center'>
-        <select size='2' value={ selectValue } onChange={ this.handleChange }>
-            <option value='rows'>rows</option>
-            <option value='columns'>columns</option>
-        </select>
-        <div grid={ selectValue } reverse horizontally-distributed='between' vertically-aligned='center'>
-          <div className='test-1'></div>
-          <div className='test'></div>
-          <div className='test-2'></div>
-          <div className='test'></div>
+      <div>
+        <button onClick={ () => dispatch(goBack()) }>Back</button>
+        <div grid='rows' horizontally-aligned='center'>
+          <select size='2' value={ selectValue } onChange={ this.handleChange }>
+              <option value='rows'>rows</option>
+              <option value='columns'>columns</option>
+          </select>
+          <div grid={ selectValue } reverse horizontally-distributed='between' vertically-aligned='center'>
+            <div className='test-1'></div>
+            <div className='test'></div>
+            <div className='test-2'></div>
+            <div className='test'></div>
+          </div>
         </div>
       </div>
     );
   }
-};
-
-export default Playground;
+}
