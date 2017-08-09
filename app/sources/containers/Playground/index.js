7f0262f94cbc50b28b4d0e632c841421701d2909
maxgraey <maxgraey@gmail.com>
2017-08-09 17:05:34 +0300

    update Pllayground

--- app/sources/containers/Playground/index.js
+++ app/sources/containers/Playground/index.js
@@ -7,31 +7,32 @@ export default class Playground extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      selectValue: 'rows',
+      selectedValue: 'rows',
     };
   }
 
   handleChange = event => {
-    this.setState({ selectValue: event.currentTarget.value });
+    this.setState({ selectedValue: event.currentTarget.value });
   }
 
   render() {
-    const { dispatch }    = this.props;
-    const { selectValue } = this.state;
+    const { dispatch }      = this.props;
+    const { selectedValue } = this.state;
     return (
       <div>
         <button onClick={ () => dispatch(goBack()) }>Back</button>
         <div grid='rows' horizontally-aligned='center'>
-          <select size='2' value={ selectValue } onChange={ this.handleChange }>
+          <select size='3' value={ selectedValue } onChange={ this.handleChange }>
               <option value='rows'>rows</option>
               <option value='columns'>columns</option>
+              <option value='masonry'>masonry</option>
           </select>
-          <div grid={ selectValue } reverse horizontally-distributed='between' vertically-aligned='center'>
-            <div className='test-1'></div>
-            <div className='test'></div>
-            <div className='test-2'></div>
-            <div className='test'></div>
-          </div>
+          <section grid={ selectedValue } vertically-aligned="center">
+            <div className='demo_box'></div>
+            <div className='demo_box'></div>
+            <div className='demo_box'></div>
+            <div className='demo_box'></div>
+          </section>
         </div>
       </div>
     );
