1ea933f31bac441ba11b455e0332ac7587071983
maxgraey <maxgraey@gmail.com>
2017-08-11 04:07:43 +0300

    minor refactor

--- app/sources/containers/Playground/index.js
+++ app/sources/containers/Playground/index.js
@@ -9,29 +9,29 @@ export default class Playground extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedValue: 'rows',
+      layout: 'rows',
     };
   }
 
   handleChange = event => {
-    this.setState({ selectedValue: event.currentTarget.value });
+    this.setState({ layout: event.currentTarget.value });
   }
 
   render() {
-    const { dispatch }      = this.props;
-    const { selectedValue } = this.state;
+    const { dispatch } = this.props;
+    const { layout } = this.state;
     return (
       <div>
         <button onClick={ () => dispatch(goBack()) }>Back</button>
         <div layout='rows' horizontal-align='center'>
 
-          <select size='3' value={ selectedValue } onChange={ this.handleChange }>
+          <select size='3' value={ layout } onChange={ this.handleChange }>
             <option value='rows'>rows</option>
             <option value='columns'>columns</option>
             <option value='masonry'>masonry</option>
           </select>
 
-          <section layout={ selectedValue } vertical-align='center' className='section-playground'>
+          <section layout={ layout } vertical-align='center' className='section-playground'>
             <div className='demo-box'></div>
             <div className='demo-box'></div>
             <div className='demo-box'></div>
