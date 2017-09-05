ef65d9150732b5028970226208b962b097d92380
maxgraey <maxgraey@gmail.com>
2017-09-05 13:09:58 +0300

    fix parallel execute root saga

--- app/sources/sagas.js
+++ app/sources/sagas.js
@@ -1,11 +1,11 @@
 
-import { fork, all } from 'redux-saga/effects'
+import { fork, all, call } from 'redux-saga/effects'
 import { appSaga } from 'containers/App/sagas'
 import { usersSaga } from 'containers/Users/sagas'
 
 export default function* rootSaga() {
   yield all([
-    appSaga,
-    usersSaga,
+    call(appSaga),
+    call(usersSaga),
   ]);
 }
