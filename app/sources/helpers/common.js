0f57b6ee873a2360215e254e0880ac801d781010
maxgraey <maxgraey@gmail.com>
2017-10-23 00:44:02 +0300

    refactor common.js

--- app/sources/helpers/common.js
+++ app/sources/helpers/common.js
@@ -6,65 +6,6 @@ const FILE_PATH_REGEX        = /[^./\\]*$/;
 const SEPARATOR_REGEX        = /[-_]/;
 
 /*
- * Runtime Type Checking
- * This useful if you don't use libs like lodash
- */
-
-export function isBoolean(arg) {
-  return typeof arg === 'boolean';
-}
-
-export function isNumber(arg) {
-  return !Array.isArray(arg) && (arg - (+arg) + 1) >= 0;
-}
-
-export function isInteger(arg, isSafe = true) {
-  return isSafe ? Number.isSafeInteger(arg) : Number.isInteger(arg);
-}
-
-export function isFunction(arg) {
-  return typeof arg === 'function';
-}
-
-export function isString(arg) {
-  return typeof arg === 'string';
-}
-
-export function isArray(arg) {
-  return Array.isArray(arg);
-}
-
-export function isTypedArray(arg) {
-  return !!arg && arg.buffer && arg.buffer.constructor === ArrayBuffer;
-}
-
-export function isObject(arg) {
-  return !!arg && typeof arg === 'object';
-}
-
-export function isDate(arg, notEmpty = false) {
-  return arg instanceof Date && !isNaN(+arg) && (notEmpty ? +arg !== 0 : true);
-}
-
-export function isRegExp(arg) {
-  return toString.call(arg) == '[object RegExp]';
-}
-
-export function isPromise(arg) {
-  return !!arg && typeof arg.subscribe !== 'function' && typeof arg.then === 'function';
-}
-
-// Usage:
-// defined(undefined, null, 0/0, 'first-valid', 123, 'foo') => 'first-valid'
-
-export function defined(...args) {
-  // return args.filter(arg => arg)[0];
-  // slightly faster
-  return args.reduce((a, b) => a || b);
-}
-
-
-/*
  * String & Formatters
  */
 
@@ -122,8 +63,95 @@ export function validateEmail(email) {
 
 
 /*
+ * Runtime Type Checking
+ */
+
+export function isBoolean(arg) {
+  return typeof arg === 'boolean';
+}
+
+export function isNumber(arg) {
+  return !Array.isArray(arg) && (arg - (+arg) + 1) >= 0;
+}
+
+export function isInteger(arg, isSafe = true) {
+  return isSafe ? Number.isSafeInteger(arg) : Number.isInteger(arg);
+}
+
+export function isFunction(arg) {
+  return typeof arg === 'function';
+}
+
+export function isString(arg) {
+  return typeof arg === 'string';
+}
+
+export function isArray(arg) {
+  return Array.isArray(arg);
+}
+
+export function isTypedArray(arg) {
+  return !!arg && arg.buffer && arg.buffer.constructor === ArrayBuffer;
+}
+
+export function isObject(arg) {
+  return !!arg && typeof arg === 'object';
+}
+
+export function isDate(arg, notEmpty = false) {
+  return arg instanceof Date && !isNaN(+arg) && (notEmpty ? +arg !== 0 : true);
+}
+
+export function isRegExp(arg) {
+  return toString.call(arg) == '[object RegExp]';
+}
+
+export function isPromise(arg) {
+  return !!arg && typeof arg.subscribe !== 'function' && typeof arg.then === 'function';
+}
+
+
+/*
+ * Objects
+ */
+
+// Usage:
+// defined(undefined, null, 0/0, 'first-valid', 123, 'foo') => 'first-valid'
+
+export function defined(...args) {
+  return args.reduce((a, b) => a || b);
+}
+
+/*
  * Arrays
  */
+
+
+// Usage:
+// compact([1,,undefined,null,NaN,5,3]) => [1,5,3]
+
+export function compact(array) {
+	if (!Array.isArray(array))
+		return [];
+
+	return array.filter(item => item);
+}
+
+
+// Usage:
+// uniq([1,2,1,2,2,1]) => [1,2]
+// Note: if you need uniq of objects and/or uniqBy/uniqWith use Ramda instead (R.uniqWith(R.identical))
+
+export function uniq(array) {
+	return Array.from(new Set(array));
+}
+
+// Usage:
+// cleanup([,,undefined,NaN,null,1,2]) => [1,2]
+
+export function cleanup(array) {
+	return uniq(compact(array));
+}
 
 // Usage:
 // shuffle([1, 2, 3, 4]) => [3, 1, 4, 2] in random order
@@ -138,14 +166,4 @@ export function shuffle(array) {
     array[randi] = temp;
   }
   return array;
-}
-
-// Usage:
-// compact([1,,undefined,null,NaN,5,3]) => [1,5,3]
-
-export function compact(array) {
-  if (!Array.isArray(array))
-    return [];
-
-  return array.filter(item => item);
 }
