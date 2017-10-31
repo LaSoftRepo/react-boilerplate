50f54fb62ed367c3da5596a9bd9a17f7a647dd47
maxgraey <maxgraey@gmail.com>
2017-10-31 02:37:41 +0200

    improve source-map generation. Now generate always full separate file map but except vendor in development

--- app/sources/containers/Playground/index.js
+++ app/sources/containers/Playground/index.js
@@ -15,6 +15,7 @@ export default class Playground extends Component {
   render() {
     const { dispatch } = this.props;
     const layout = this.state.layout;
+
     return (
       <div>
         <button onClick={ () => dispatch(goBack()) }>Back</button>
